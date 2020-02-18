900f17d657b9632ca25c0eaadb6677e88c958ba1
Mike Warren <mikeywarren89@gmail.com>
2020-02-18 13:34:40 +0900

    added all controller actions and front end improvements, able to use as to-do list

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,56 @@
+class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy, :check]
+
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+
+    if @task.save
+      redirect_to tasks_path
+    else
+      render :new
+    end
+  end
+
+  def check
+    @task.completed = !@task.completed
+    @task.save
+    redirect_back(fallback_location: tasks_path)
+  end
+
+  def edit
+  end
+
+  def update
+    if @task.update(task_params)
+      redirect_to tasks_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # read
+  get "/tasks", to: "tasks#index", as: :tasks
+  # need this new higher than :id otherwise error
+  get "/tasks/new", to: "tasks#new", as: :new_task
+  # see above
+  get "tasks/:id/check", to: "tasks#check", as: :check_task
+  get "/tasks/:id", to: "tasks#show", as: :task
+  # create
+  post "/tasks", to: "tasks#create"
+  # update
+  get "/tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  patch "/tasks/:id", to: "tasks#update"
+  # destroy
+  delete "/tasks/:id", to: "tasks#destroy"
 end
